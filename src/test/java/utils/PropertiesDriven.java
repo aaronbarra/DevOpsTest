615f8666babb963af38b8ed2374db515fa543587
Aaron Barra <aaron.barra@tsoftglobal.com>
2023-01-01 04:51:36 -0300

    update directory v2

--- src/test/java/utils/PropertiesDriven.java
+++ src/test/java/utils/PropertiesDriven.java
@@ -10,7 +10,7 @@ public class PropertiesDriven {
 
     public static String getProperty(String key){
         props = new Properties();
-        String rutaFile = "https://github.com/aaronbarra/DevOpsTest/blob/0b750faccce04792f0ba9ad5467397913c2edb14/src/test/resources/Properties.properties";
+        String rutaFile = "src/test/resources/Properties.properties";
         try {
             InputStream input = new FileInputStream(rutaFile);
             props.load(input);
