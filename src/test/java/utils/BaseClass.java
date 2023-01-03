db84ea240272edbc78049f4da50fc8a82e74540e
Aaron Barra <aaron.barra@tsoftglobal,con>
2023-01-02 23:01:52 -0500

    Update webdriver

--- src/test/java/utils/BaseClass.java
+++ src/test/java/utils/BaseClass.java
@@ -42,7 +42,7 @@ public class BaseClass {
     public WebDriver conexionBrowser(String browser,String propertyDriver,String rutaDriver){
         switch (browser){
             case "CHROME":
-                System.setProperty(propertyDriver,rutaDriver);
+                System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
                 this.driver = new ChromeDriver(new ChromeDriverService.Builder().usingPort(65535).build());
                 return this.driver;
             case "EDGE":
